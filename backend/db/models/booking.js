93dbd4197595f927d7ca2b50efbb31687b7e156f
Joseph Dumas <josephdumas@CHIDRLPF5JKYY2.localdomain>
2022-12-28 22:40:52 -0500

    revised references in booking model

--- backend/db/models/booking.js
+++ backend/db/models/booking.js
@@ -25,7 +25,7 @@ module.exports = (sequelize, DataTypes) => {
       type: DataTypes.INTEGER,
       allowNull: false,
       references: {
-        model: 'User',
+        model: User,
         key: 'id'
       }
     },
