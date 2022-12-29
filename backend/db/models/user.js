72fa608a6c75561e141aedfdcfc528bb13952581
Joseph Dumas <josephdumas@CHIDRLPF5JKYY2.localdomain>
2022-12-29 14:15:22 -0500

    additional changes to user model

--- backend/db/models/user.js
+++ backend/db/models/user.js
@@ -41,10 +41,10 @@ module.exports = (sequelize, DataTypes) => {
     }
     static associate(models) {
       // define association here
-      // User.hasMany(models.Spot, { as: "Owner", foreignKey: 'ownerId' });
+      User.hasMany(models.Spot, { as: "Owner", foreignKey: 'ownerId' });
       User.hasMany(models.Review, { foreignKey: 'userId' });
-      // User.hasMany(models.Booking, { foreignKey: 'userId'} );
-      User.belongsToMany(models.Spot, { through: models.Booking, foreignKey: 'userId' });
+      User.hasMany(models.Booking, { foreignKey: 'userId'} );
+      // User.belongsToMany(models.Spot, { through: models.Booking, foreignKey: 'userId' });
       User.belongsToMany(models.Spot, { through: models.Review, foreignKey: 'userId' });
     }
   };
