3d4b243c74f056d3a37bb8aff1327d0ad5db22de
Joseph Dumas <josephdumas@CHIDRLPF5JKYY2.localdomain>
2023-01-13 19:19:03 -0500

    converted login form page into modal

---
+++ frontend/src/components/LoginFormModal/index.js
@@ -0,0 +1,63 @@
+import React, { useState } from 'react';
+import * as sessionActions from '../../store/session';
+import { useDispatch } from 'react-redux';
+import { useModal } from '../../context/Modal';
+import './LoginForm.css';
+
+function LoginFormModal() {
+  const dispatch = useDispatch();
+  const [credential, setCredential] = useState('');
+  const [password, setPassword] = useState('');
+  const [errors, setErrors] = useState([]);
+  const { closeModal } = useModal();
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    setErrors([]);
+    return dispatch(sessionActions.login({ credential, password }))
+      .then(closeModal)
+      .catch(
+        async (res) => {
+          const data = await res.json();
+          if (data && data.errors) setErrors(data.errors);
+        }
+      );
+  };
+
+  return (
+    <>
+      <h1>Log In</h1>
+      <form className="login-form" onSubmit={handleSubmit}>
+        <ul>
+          {errors.map((error, idx) => <li key={idx}>{error}</li>)}
+        </ul>
+        <label className="cred-label">
+          Username or Email:
+          <br />
+          <input
+            type="text"
+            value={credential}
+            onChange={(e) => setCredential(e.target.value)}
+            required
+          />
+        </label>
+        <label className="password-label">
+          Password:
+          <br />
+          <input
+            type="password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+            required
+          />
+        </label>
+        <br />
+        <button className="login-btn" type="submit">Log In</button>
+        <br />
+      </form>
+    
+    </>
+  );
+}
+
+export default LoginFormModal;
