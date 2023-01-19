20f838f2b4359e7d5cd8409fe8ff51f0d572f306
Joseph Dumas <josephdumas@CHIDRLPF5JKYY2.localdomain>
2023-01-19 14:47:18 -0500

    created reviews reducer

---
+++ frontend/src/store/reviews.js
@@ -0,0 +1,17 @@
+import { csrfFetch } from "./csrf";
+
+// ACTION CREATORS
+
+// THUNK ACTION CREATORS
+
+// REDUCER
+const initialState = {};
+
+const reviewsReducer = (state = initialState, action) => {
+    switch (action.type) {
+        default:
+            return state;
+    }
+}
+
+export default reviewsReducer;
